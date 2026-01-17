77829ca99678062e3a9e98a295fe0b21b8af236f
zeeshan-890 <zzabbaskhan830@gmail.com>
2026-01-17 20:34:22 +0500

    fix : fb cdn imag issue

--- next.config.js
+++ next.config.js
@@ -24,6 +24,10 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
+                hostname: '**.fbcdn.net',
+            },
+            {
+                protocol: 'https',
                 hostname: '**.cdninstagram.com',
             },
             {
